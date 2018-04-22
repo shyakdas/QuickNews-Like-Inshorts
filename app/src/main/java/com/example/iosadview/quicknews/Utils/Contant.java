cdc655a4439a75de3fcf1f4a20c91c8cc09311ee
Shyak Das <shayak@adview.com>
2018-04-22 18:58:34 +0530

    [Implementation]Naviagtion ICON

--- app/src/main/java/com/example/iosadview/quicknews/Utils/Contant.java
+++ app/src/main/java/com/example/iosadview/quicknews/Utils/Contant.java
@@ -5,4 +5,6 @@ public class Contant {
     public static final String COUNTRY = "country";
     public static final String CATEGORY = "category";
     public static final String API_KEY = "apiKey";
+    private static final String TAG_MASTER_FRAGMENT = "TAG_MASTER_FRAGMENT";
+    private static final String TAG_DETAIL_FRAGMENT = "TAG_DETAIL_FRAGMENT";
 }
--- app/src/main/java/com/example/iosadview/quicknews/home/MainActivity.java
+++ app/src/main/java/com/example/iosadview/quicknews/home/MainActivity.java
@@ -3,8 +3,13 @@ package com.example.iosadview.quicknews.home;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.constraint.ConstraintLayout;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.MenuItem;
 
 import com.example.iosadview.quicknews.Interface.PostItemClickListener;
 import com.example.iosadview.quicknews.R;
@@ -21,6 +26,7 @@ public class MainActivity extends MvpLceActivity<ConstraintLayout, BaseResponse,
     private VerticalViewPager verticalViewPager;
     private VerticlePagerAdapter verticlePagerAdapter;
     private ArrayList<BaseResponse.Article> mTotoalList;
+    private DrawerLayout mDrawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +34,24 @@ public class MainActivity extends MvpLceActivity<ConstraintLayout, BaseResponse,
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+        if (mDrawerLayout != null) { // <------ add if
+            // setup drawer view
+            NavigationView navigationView = findViewById(R.id.master_fragment_container);
+            navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    return true;
+                }
+            });
+
+            // setup menu icon
+            final ActionBar actionBar = getSupportActionBar();
+            if (actionBar != null) {
+                actionBar.setHomeAsUpIndicator(R.drawable.ic_menu_black_24dp);
+                actionBar.setDisplayHomeAsUpEnabled(true);
+            }
+        }
         mTotoalList = new ArrayList<>();
         verticalViewPager = findViewById(R.id.verticleViewPager);
         verticlePagerAdapter = new VerticlePagerAdapter(this, mTotoalList, this);
@@ -71,5 +95,17 @@ public class MainActivity extends MvpLceActivity<ConstraintLayout, BaseResponse,
     public void postClick(int position, String url) {
         Log.e("TAG", "checkingTheClick===" + position + "url====" + url);
         AppUtis.creatorCustomTabs(this, url);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // The action bar home/up action should open or close the drawer.
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
